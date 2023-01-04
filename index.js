027f043de5549be004a6fcb549d7f3669ccb657d
jeanramalho <jeanramalho.dev@gmail.com>
2023-01-04 11:38:22 -0300

    Instala o handlebars

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express()
+const handlebars = require('express-handlebars')
+
+
 
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081")
