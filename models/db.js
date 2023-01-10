905ee7949cf869a2b0a79abde87dd676be57a13c
jeanramalho <jeanramalho.dev@gmail.com>
2023-01-10 15:49:23 -0300

    Muda configuração de banco de dados

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
   // Conexão com banco de dados
-  const sequelize = new Sequelize('test', 'root', '123456', {
+  const sequelize = new Sequelize('postapp', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql'
 })
