088c579f117e765a50a3265f1832f11dd599ef40
jeanramalho <jeanramalho.dev@gmail.com>
2023-01-11 14:45:31 -0300

    Importa Model Post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-
+const Post = require('./models/Post')
 
 // Config
     // Template Engine
